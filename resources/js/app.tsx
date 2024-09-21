682a7815a66b3f748af684134bc484af88da0823
Luis Güette <guetteluis@gmail.com>
2024-09-21 14:15:02 -0400

    Fix formatting issues

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,15 +1,15 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 const pagePath = import.meta.glob('./Pages/**/*.tsx');
 
 createInertiaApp({
-  title: (title) => `${title ? title + ' - ' : ' '}${appName}`,
+  title: (title) => `${title ? `${title} - ` : ' '}${appName}`,
   resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, pagePath),
   setup({ el, App, props }) {
     const root = createRoot(el);
