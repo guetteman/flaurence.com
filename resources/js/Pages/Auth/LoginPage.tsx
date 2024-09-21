0139cdace7e596a6903df39acf67f48b429583fe
Luis Güette <guetteluis@gmail.com>
2024-09-21 13:25:00 -0400

    Add login page

--- resources/js/Pages/Auth/LoginPage.tsx
+++ resources/js/Pages/Auth/LoginPage.tsx
@@ -1,8 +1,110 @@
+import { Button } from '@/components/ui/button';
+import {
+  FormControl,
+  FormError,
+  FormField,
+  FormLabel,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Link, useForm } from '@inertiajs/react';
+import { Scissors } from 'lucide-react';
+import React, { type FormEvent } from 'react';
+
 export default function LoginPage() {
+  const { data, setData, errors, post, processing } = useForm({
+    email: '',
+    password: '',
+    remember: false,
+  });
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    post('/login');
+  }
+
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Login</h1>
-      <p>This is a login page</p>
+    <div className="flex min-h-screen">
+      <div className="hidden w-1/2 bg-black text-white lg:block">
+        <div className="flex h-full flex-col justify-between p-8">
+          <div className="flex items-center space-x-2">
+            <Scissors className="size-8" />
+            <span className="text-xl font-bold">Flaurence</span>
+          </div>
+          <div className="max-w-2xl">
+            <blockquote>
+              "This library has saved me countless hours of work and helped me
+              deliver stunning designs to my clients faster than ever before."
+            </blockquote>
+            <cite className="mt-2 block">Sofia Davis</cite>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-full items-center justify-center lg:w-1/2">
+        <div className="mx-auto w-full max-w-md space-y-8 px-4">
+          <div className="text-center">
+            <h2 className="text-4xl font-bold">Login</h2>
+            <p className="mt-2 text-sm text-gray-600">
+              Enter your email below to login your account
+            </p>
+          </div>
+          <form className="space-y-1" onSubmit={handleSubmit}>
+            <FormField>
+              <FormLabel htmlFor="email">Email Address</FormLabel>
+              <FormControl>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={data.email}
+                  onChange={(e) => setData('email', e.target.value)}
+                  placeholder="name@example.com"
+                  required
+                />
+              </FormControl>
+              <FormError error={errors.email} />
+            </FormField>
+            <FormField>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <FormControl>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="Enter your password"
+                  value={data.password}
+                  onChange={(e) => setData('password', e.target.value)}
+                  required
+                />
+              </FormControl>
+              <FormError error={errors.password} />
+            </FormField>
+            <Button
+              type="submit"
+              className="w-full bg-black text-white hover:bg-gray-800"
+            >
+              Sign In
+            </Button>
+          </form>
+          <div className="text-center text-sm">
+            Don't have an account?{' '}
+            <Link href="/register" className="font-medium underline">
+              Register here
+            </Link>
+          </div>
+          {/*<p className="text-center text-sm text-gray-600">*/}
+          {/*  By clicking continue, you agree to our{' '}*/}
+          {/*  <Link href="#" className="underline">*/}
+          {/*    Terms of Service*/}
+          {/*  </Link>{' '}*/}
+          {/*  and{' '}*/}
+          {/*  <Link href="#" className="underline">*/}
+          {/*    Privacy Policy*/}
+          {/*  </Link>*/}
+          {/*  .*/}
+          {/*</p>*/}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ resources/js/components/ui/form.tsx
@@ -0,0 +1,34 @@
+import { Label } from '@/components/ui/label';
+import React, { type ReactNode } from 'react';
+
+function FormField({ children }: { children: ReactNode }) {
+  return <div className="space-y-1">{children}</div>;
+}
+
+function FormLabel({
+  htmlFor,
+  children,
+}: {
+  htmlFor: string;
+  children: ReactNode;
+}) {
+  return (
+    <Label htmlFor={htmlFor} className="px-1">
+      {children}
+    </Label>
+  );
+}
+
+function FormControl({ children }: { children: ReactNode }) {
+  return <div>{children}</div>;
+}
+
+function FormError({ error }: { error?: string }) {
+  return (
+    <div className="min-h-4 px-1">
+      {error && <div className="text-xs text-red-500">{error}</div>}
+    </div>
+  );
+}
+
+export { FormField, FormLabel, FormControl, FormError };
