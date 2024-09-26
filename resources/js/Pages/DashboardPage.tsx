d17edd7e30442ddab8cfd686edc969f499a3ae03
Luis Güette <guetteluis@gmail.com>
2024-09-25 21:32:17 -0400

    Add animations

--- resources/js/Pages/DashboardPage.tsx
+++ resources/js/Pages/DashboardPage.tsx
@@ -2,29 +2,36 @@ import { Navbar } from '@/components/app/navbar';
 import { EmptyProjects } from '@/components/app/pages/dashboard/empty-projects';
 import { PlusSignIcon } from '@/components/icons/plus-sign-icon';
 import { Button } from '@/components/ui/button';
+import { AnimatedContainer } from '@/components/ui/layout/animated-container';
 import { Separator } from '@/components/ui/separator';
+import { useAnimationVariants } from '@/hooks/use-animation-variants';
 import type { ProjectResourceCollection } from '@/types/projects';
+import { motion } from 'framer-motion';
 
 interface DashboardPageProps {
   projects: ProjectResourceCollection;
 }
 
 export default function DashboardPage({ projects }: DashboardPageProps) {
+  const { slideUpInVariants } = useAnimationVariants();
   return (
-    <div className="flex min-h-screen flex-col">
+    <AnimatedContainer className="flex min-h-screen flex-col">
       <Navbar />
       <div className="mx-auto mt-24 flex w-full max-w-7xl flex-col px-4">
-        <div className="flex items-center justify-end">
+        <motion.div
+          variants={slideUpInVariants}
+          className="flex items-center justify-end"
+        >
           <Button>
             <PlusSignIcon className="mr-2 size-4" />
             New Project
           </Button>
-        </div>
+        </motion.div>
         <Separator className="mt-8" />
         <div>
           {projects.data.length ? <div>Projects</div> : <EmptyProjects />}
         </div>
       </div>
-    </div>
+    </AnimatedContainer>
   );
 }
--- resources/js/components/app/pages/dashboard/empty-projects.tsx
+++ resources/js/components/app/pages/dashboard/empty-projects.tsx
@@ -1,21 +1,27 @@
 import { FolderAddIcon } from '@/components/icons/folder-add-icon';
 import { PlusSignIcon } from '@/components/icons/plus-sign-icon';
 import { Button } from '@/components/ui/button';
+import { useAnimationVariants } from '@/hooks/use-animation-variants';
+import { motion } from 'framer-motion';
 
 export function EmptyProjects() {
+  const { slideUpInVariants } = useAnimationVariants();
   return (
-    <div className="flex h-96 flex-col justify-center text-center">
+    <motion.div
+      variants={slideUpInVariants}
+      className="flex h-96 flex-col justify-center text-center"
+    >
       <FolderAddIcon className="mx-auto size-12 text-gray-400" />
       <h3 className="mt-2 text-sm font-semibold text-gray-900">No projects</h3>
       <p className="mt-1 text-sm text-gray-500">
         Get started by creating a new project.
       </p>
-      <div className="mt-6">
+      <motion.div variants={slideUpInVariants} className="mt-6">
         <Button>
           <PlusSignIcon aria-hidden="true" className="mr-2 size-4" />
           New Project
         </Button>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
